43fe9bcf9995305e475c093eb2b964c7abb3b170
djfdmator <43840175+djfdmator@users.noreply.github.com>
2021-08-25 18:52:21 +0900

    Intro UI

---
+++ Mokoko_Adventure/Source/Mokoko_Adventure/Public/MAIntroWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MAIntroWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOKOKO_ADVENTURE_API UMAIntroWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
